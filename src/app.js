a1eb4888638dd4890565260f8ed3a8bcdf3db69a
scoobaroo <scoobaroo415@gmail.com>
2016-10-22 12:19:13 -0700

    fixing app.js loading

--- src/app.js
+++ src/app.js
@@ -2,22 +2,16 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 var app = document.getElementById("app");
 
-import React from 'react/addons';
+console.log("app.js loaded");
 
-class UserName extends React.Component {
-  render() {
-    return <div>name: {this.props.name}</div>;
-  }
-}
-
-class User extends React.Component {
+var User = React.createClass({
   render() {
     return <div>
-        <h1>City: {this.props.user.city}</h1>
+        <h1>City: {this.props.user.city} </h1>
         <UserName name={this.props.user.name} />
       </div>;
   }
-}
+});
 // tutorial1.js
 var CommentBox = React.createClass({
   render: function() {
