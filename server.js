32e4381e9eb57174c9931da31200b2aef744a835
scoobaroo <scoobaroo415@gmail.com>
2016-10-31 00:47:13 -0700

    added stormpath to server and app.js for authentication

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 //Lets load the mongoose module in our program
 var mongoose = require('mongoose');
+var stormpath = require('express-stormpath');
+var webpack = require('webpack');
+var config = require('./webpack.config');
 var express = require('express');
 var app = express();
+var path = require('path');
+var compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
 // SERVER-SIDE JAVASCRIPT
 var db = require('./models');
 var bodyParser = require('body-parser');
@@ -10,28 +21,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/vendor', express.static(__dirname + '/bower_components'));
 //Lets connect to our database using the DB server URL.
 db = mongoose.createConnection('mongodb://localhost/gratitude');
-
 // prepare server
 app.use('/', express.static(__dirname + '/www')); // redirect root
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
+app.use(stormpath.init(app, {
+  web: {
+    produces: ['application/json']
+  }
+}));
 
+app.get('/css/bootstrap.min.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'node_modules/bootstrap/dist/css/bootstrap.min.css'));
+});
 
-/**
- * Lets define our Model for User entity. This model represents a collection in the database.
- * We define the possible schema of User document and data types of each field.
- * */
-/**
- * Lets Use our Models
- * */
-
-
-app.get('/', function homepage (req, res) {
+app.get('*', function homepage (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Express server is running on http://localhost:3000/');
+app.on('stormpath.ready', function () {
+  app.listen(3000, 'localhost', function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('Listening at http://localhost:3000');
+  });
 });
--- src/app.js
+++ src/app.js
@@ -2,13 +2,34 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 var app = document.getElementById("app");
+var stormpath = require('express-stormpath');
+var ReactStormpath = require('react-stormpath').default;
+var AuthenticatedRoute = require('react-stormpath').AuthenticatedRoute;
+var LoginLink = require('react-stormpath').LoginLink;
+
+import { Router } from 'react-stormpath';
+import { Route, browserHistory } from 'react-router';
 
 console.log("app.js loaded");
+
+<Route path='/register' component={RegistrationPage} />
+<LoginRoute path='/login' component={LoginPage} />
+<AuthenticatedRoute path='/protected' component={ProtectedPage} />
+<HomeRoute path='/' component={HomePage} />
+<AuthenticatedRoute>
+  <HomeRoute path='/profile' component={ProfilePage} />
+</AuthenticatedRoute>
 
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
+        <NotAuthenticated>
+          <LoginLink>Login</LoginLink>
+        </NotAuthenticated>
+        <Authenticated>
+          <LogoutLink>Logout</LogoutLink>
+        </Authenticated>
         <h1>Gratitude List</h1>
         Hello, world! I am a CommentBox.
         <Form />
@@ -75,6 +96,7 @@ class Form extends React.Component {
   }
 }
 
+ReactStormpath.init();
 ReactDOM.render(
   <CommentBox />,
   document.getElementById('app')
